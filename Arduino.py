5923e85158c6bfbe2627412e068b9afb33ebe368
eizasai <94396806+eizasai@users.noreply.github.com>
2021-11-29 14:37:05 -0500

    Update Arduino.py

--- Arduino.py
+++ Arduino.py
@@ -1,17 +1,13 @@
 import serial
 from datetime import datetime
 
-arduino = serial.Serial('COM4', 115200)
+arduino = serial.Serial('COM4', 115200) #check bluetooth setting/more bluetooth options/coms and change the com# to the output connection
 filename = "analog-data.csv"
 open(filename, "w").write("")
 samples = 432000
 file = open(filename, "a")
 file.write("time, HR, accelerometer1x, accelerometer1y, accelerometer1z, accelerometer2x, accelerometer2y, accelerometer2z, strain guage\n")
 line = 0
-def cane_status():
-    if(not arduino.available()):
-        return False;
-    
     
 while line < samples:
     getData = str(arduino.readline())
