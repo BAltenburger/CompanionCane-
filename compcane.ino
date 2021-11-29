5fcd92c01f3d61d1feb5c625516dcab037b7cd94
eizasai <94396806+eizasai@users.noreply.github.com>
2021-11-29 15:04:29 -0500

    Update compcane.ino

--- compcane.ino
+++ compcane.ino
@@ -11,54 +11,53 @@
 #if !defined(CONFIG_BT_ENABLED) || ! defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run 'make menuconfig' to and enable it
 #endif
-BluetoothSerial SerialBT;
-Adafruit_ADXL345_Unified a1(0x68);
-Adafruit_ADXL345_Unified a2(0x69);
+BluetoothSerial SerialBT;    //creates object for serial bluetooth connect
+Adafruit_ADXL345_Unified a1  //creates object for the accelerometer. right now
 HX711 lc;
-const int pin = A0;
+const int pin = 34;  //pint defaults to A0 but for wolfgang's circuit it is on the GPIO34
 const int th = 550;
 byte samplesUntilReport;
 const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
-PulseSensorPlayground pS;
-unsigned long cm;
+PulseSensorPlayground pS; //creates the pulse sensor object, pS
+unsigned long cm;    //these two longs are used to track the time passed to read data every 200 ms
 unsigned long sm;  
 void setup(){
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test");
+  Serial.begin(115200);   //starts serial with baudrate of 115200. when testing using serial monitor make sure to set the baudrate to this value along with any python code
+  SerialBT.begin("ESP32"); //this sets the name of the device and makes it discoverable via bluetooth
   
-  pS.analogInput(pin);
-  pS.setSerial(Serial);
-  pS.setThreshold(th);
-  pS.setOutputType(SERIAL_PLOTTER);
-  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
-  pS.begin();
-  a1.begin();
-  a2.begin();
-  a1.setRange(ADXL345_RANGE_4_G);
-  a2.setRange(ADXL345_RANGE_4_G);
-  lc.begin(DOUT, CLK);
-  lc.set_scale(calibration_factor);
-  sm = millis();
+  pS.analogInput(pin);   //sets pulse sensor input to be from pin 34
+  pS.setSerial(Serial);  //sets pulse sensor serial to the serial set before and will later output raw values that can be viewed in the serial monitor
+  pS.setThreshold(th);   //sets pulse sensor threshold
+  pS.setOutputType(SERIAL_PLOTTER);  //sets output type which basically graphs the raw values from the pulse sensor which can viewed from the serial plotter menu
+  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE; //sets number of samples before calculating heart rate
+  pS.begin();  //starts the pulseSensor
+  a1.begin();  //starts the accelerometer
+  //a2.begin(); //commented out for now since aryan will figure out how to differentiate data from the 2 accelerometers
+  a1.setRange(ADXL345_RANGE_4_G);  //sets the range of the accelerometer
+  //a2.setRange(ADXL345_RANGE_4_G); //commented out for now since aryan will figure out how to differentiate data from the 2 accelerometers
+  lc.begin(DOUT, CLK);  //starts the load cell
+  lc.set_scale(calibration_factor);  //calibrates the load cell based on the calibration factor. I have not calibrated it and am just using a default value that i found online
+  sm = millis();  //this sets the start time of the code and is later updated to keep track of every interval of 200ms
 }
 void loop(){
-  cm = millis();
-  sensors_event_t event;
-  int bpm = pS.getBeatsPerMinute();
-  if(Serial.available()){
+  cm = millis();  //this keeps track of the time in each interval of the loop and is later used and compared with the start time variable "sm" to see if 200ms passed
+  sensors_event_t event;  //this is used in tandem with the accelerometer to get the "event" data
+  int bpm = pS.getBeatsPerMinute();  //this gets the bpm calculated from the esp32 and pulse sensor data
+  if(Serial.available()){    //this section sets the serial connection up for both the bluetooth and wired connection if there is a wired connection.
     SerialBT.write(Serial.read());
   }
   if(SerialBT.available()){
     Serial.write(SerialBT.read());
   }
-  if(pS.sawNewSample()){
-    if(--samplesUntilReport == (byte) 0){
+  if(pS.sawNewSample()){ //checks if pulse sensor has read a pulse and outputs raw data
+    if(--samplesUntilReport == (byte) 0){ 
       samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
       pS.outputSample();
       if(pS.sawStartOfBeat()){
         pS.outputBeat();
       }
     }
-    if(cm - sm>= 200){
+    if(cm - sm>= 200){  //checks to see if 200ms passed and prints all the values to the bluetooth serial that is then read by the python code
       a1.getEvent(&event);
       SerialBT.print(bpm);
       SerialBT.print(", ");
