8c4d17846fa75975f61a18a31658b0c3b71e87d2
BAltenburger <76670222+BAltenburger@users.noreply.github.com>
2021-12-04 17:36:19 -0500

    Add files via upload

--- fall_detection.py
+++ fall_detection.py
@@ -3,49 +3,57 @@ from matplotlib import pyplot as plt
 import time
 
 def fall_detection(csv):
-    return accelerometer_fall(csv,1) and not is_force(csv) and not is_heart_rate(csv)
+    return not is_heart_rate(csv) and accelerometer_fall(csv,1) and not is_force(csv)
 
 
 def accelerometer_fall(csv, num):
     x = " accelerometer" + str(num) + "x"
     y = " accelerometer" + str(num) + "y"
     z = " accelerometer" + str(num) + "z"
+    fall_check = False
+    second_fall_check_count = 0
+    timestamp = None
     index = 0
-        
+    
     for i in csv[z]:
-#        if float(i) == float(csv[y][index]) == float(csv[z][index]):
-#            return True
-#        index += 1
-        if i == 0 or i <= 1.5:
-            return True
+        if 0<= i <= 1.5:
+            fall_check = True
+            timestamp = csv["time"][index]
+        
+            time.sleep(10)
+            second_index = 0
+            for x in range(0,49):
+                if 0 <= csv[z][second_index - x] <= 1.5:
+                    second_fall_check_count += 1
+                second_index += 1
+            index += 1
 
+            
+
+    if not(second_fall_check_count == 0) and fall_check == True:
+        fall_check = True
+        
+    else:
+        fall_check = False
+        timestamp = None
+        
+    print(timestamp)
+    return fall_check
+
+
+def is_force(csv):
+    for i in csv[" strain guage"]:
+        if i == 0:
+            return True
     return False
 
-def is_force(csv1):
-    i = -1
-    while i < csv[" strain guage"].index:
-        if csv[" strain guage"][i-1] != 0:
-            return False
-        i += 1
-    return True
-
-def is_heart_rate(csv1):
+def is_heart_rate(csv):
     time.sleep(30)
 
     for i in range(len(csv[" HR"]) - 150, len(csv[" HR"])):
-        if csv[" HR"] != 0:
-            return True
-    return False
-
-def hr_checker():
-	counter = 0
-	age = 19 #open(“user_age.txt”, ‘r’).read()
-	for i in csv["time"]:
-		if i > (220 - age) * 0.93:
-			counter += 1
-		if counter >= 150:
-			return True
-	return False
+        if csv[" HR"][i] == 0:
+            return False
+    return True
 
 df = pd.read_csv("data.csv", parse_dates = True)
 print(fall_detection(df))
