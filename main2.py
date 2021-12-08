f17a15ec8f4a919cafb005334491750b973b8609
eizasai <94396806+eizasai@users.noreply.github.com>
2021-12-08 14:08:03 -0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,48 +5,59 @@ import emailsender
 from sms import sms
 import time
 import pandas as pd
-import graphing
+#import graphing
 from os.path import exists
 import threading
 
-schedule.every().day.at("21:59").do(graphing.plot_today)
-schedule.every().day.at("22:00").do(emailsender.emaildaily)
-schedule.every().monday.do(graphing.plot_week)
-schedule.every().monday.do(emailsender.emailweek)
-
+# schedule.every().day.at("21:59").do(graphing.plot_today)
+# schedule.every().day.at("22:00").do(emailsender.emaildaily)
+# schedule.every().monday.do(graphing.plot_week)
+# schedule.every().monday.do(emailsender.emailweek)
+# def count(count):
+#     while count>0:
+#         time.sleep(1)
+#         print(count)
+#         count-=1
 def check():  
-    Cane_fall=False
+    Cane_fall=True
     Cane_fall2=False
     sc=0
     oc=0
     fg=0
     #hr=0
-    index=0
-    while thread1.is_alive():
-        compressed=pd.read_csv("new_data.csv",chunksize=100000,parse_dates = True)
+    index=26373
+    # while thread1.is_alive():
+    while True:
+        compressed=pd.read_csv("data.csv",chunksize=100000,parse_dates = True)
         csv=pd.concat(compressed)
         length=len(csv.index)
-        l=length-1
+        # l=length-1
+        l=index+300
         if Cane_fall and not Cane_fall2:
             l3=l
             l2=index-300
             fc=0
             while l2<index:
-                f=csv.iloc[l2][" strain guage"]
-                if abs(f)>1.9:
+                f=float(csv.iloc[l2][" strain guage"])
+                if abs(f)>3:
                     fc+=1
-                if fc>50:
-                    l-=600
-                    l2+=300
-                    Cane_fall2=True  
-                    print("fall stage 1 cleared")
+                # if fc>50:
+                #     l2+=300
+                #     Cane_fall2=True  
+                #     print("fall stage 1 cleared")
                 l2+=1
+            if fc>50:
+                l2+=300
+                print(fc)
+                Cane_fall2=True  
+                print("fall stage 1 cleared")
             if not Cane_fall2:
+                print("fc:"+str(fc))
                 Cane_fall=False
                 print("fall cancelled")
-            else:
-                time.sleep(15)
-        if Cane_fall2 and Cane_fall and l>l3:                
+            # else:
+            #     time.sleep(15)
+        if Cane_fall2 and Cane_fall and l>index:                
             while l>index and l>3:
                 b2=csv.iloc[l][" accelerometer1x"]
                 b1=csv.iloc[l-1][" accelerometer1x"]
@@ -54,62 +65,71 @@ def check():
                 c1=csv.iloc[l-1][" accelerometer1y"]
                 d2=csv.iloc[l][" accelerometer1z"]
                 d1=csv.iloc[l-1][" accelerometer1z"]
-                f=csv.iloc[l][" strain guage"]
+                f=float(csv.iloc[l][" strain guage"])
                 #h=csv.iloc[l][" HR"]
                 bm=b1-b2
                 cm=c1-c2
                 dm=d1-d2
-                th=0.5
+                th=0.15
                 if bm<th and cm<th and dm<th:
                     sc+=1
-                if b2<3:
+                if b2<0.5:
                     oc+=1     
-                if abs(f)>1.9:
+                if abs(f)>3.5:
                     fg+=1
                 # if h>40:
                 #     hr+=1
-                if fg>50: #or hr>100:
-                    Cane_fall=False
-                    l-=600
-                    print("fall detection cancelled")
+                # if fg>50: #or hr>100:
+                #     Cane_fall=False
+                #     l-=600
+                #     print("fall detection cancelled2")
                 l-=1
-            if (sc>199 or oc>199) and Cane_fall:
-                graphing
+            if fg>50: #or hr>100:
+                Cane_fall=False
+                l-=600
+                print("fall detection cancelled2")    
+            print("fg:"+str(fg))
+            print("oc:"+str(oc))
+            print("sc:"+str(sc))
+            if (sc>149 or oc>149) and Cane_fall:
+                # graphing
                 print("fall confirmed")
-                sms("Fall detected")
-                emailsender.emailtool("Fall data")
+                # sms("Fall detected")
+                # emailsender.emailtool("Fall data")
+            elif not fg>50:
+                print("fall detection cancelled3")
             sc=0
             oc=0
             fg=0
             Cane_fall=False
             Cane_fall2=False
-        while l>3 and l>length-40:
-            b2=csv.iloc[l][" accelerometer1x"]
-            b1=csv.iloc[l-1][" accelerometer1x"]
-            c2=csv.iloc[l][" accelerometer1y"]
-            c1=csv.iloc[l-1][" accelerometer1y"]
-            d2=csv.iloc[l][" accelerometer1z"]
-            d1=csv.iloc[l-1][" accelerometer1z"]
-            bm=b1-b2
-            cm=c1-c2
-            dm=d1-d2
-            if not bm-cm==0:
-                r1=(b1-c1)/(cm-bm)
-            else:
-                r1=0
-            if not cm-dm==0:
-                r2=(c1-d1)/(dm-cm)
-            else:
-                r2=0
-            if r1<=1 and r2<=1:
-                if (r1>0 and r2>0) or (b1==c1 and b1==d1):
-                    print(l)
-                    print("fall detected")
-                    index=l
-                    Cane_fall=True
-                    l-=40
-            l-=1
-        time.sleep(0.5)
+        # while l>3: #and l>length-40:
+        #     b2=csv.iloc[l][" accelerometer1x"]
+        #     b1=csv.iloc[l-1][" accelerometer1x"]
+        #     c2=csv.iloc[l][" accelerometer1y"]
+        #     c1=csv.iloc[l-1][" accelerometer1y"]
+        #     d2=csv.iloc[l][" accelerometer1z"]
+        #     d1=csv.iloc[l-1][" accelerometer1z"]
+        #     bm=b1-b2
+        #     cm=c1-c2
+        #     dm=d1-d2
+        #     if not bm-cm==0:
+        #         r1=(b1-c1)/(cm-bm)
+        #     else:
+        #         r1=0
+        #     if not cm-dm==0:
+        #         r2=(c1-d1)/(dm-cm)
+        #     else:
+        #         r2=0
+        #     if r1<=1 and r2<=1:
+        #         if (r1>0 and r2>0) or (b1==c1 and b1==d1):
+        #             print(l)
+        #             print("fall detected")
+        #             index=l
+        #             #Cane_fall=True
+        #             l-=40
+        #     l-=1
+        # time.sleep(0.5)
         
 def cane():
     arduino=serial.Serial('COM6',115200)
@@ -131,7 +151,8 @@ def cane():
         schedule.run_pending()    
     arduino.close()
 
-thread1=threading.Thread(target=cane)
-thread2=threading.Thread(target=check)
-thread1.start()
-thread2.start()
+# thread1=threading.Thread(target=cane)
+# thread2=threading.Thread(target=check)
+# thread1.start()
+# thread2.start()
+check()
