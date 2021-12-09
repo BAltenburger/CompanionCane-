7b5a4ac9de1b240a3c9a45aae8426b0aab040a0d
eizasai <94396806+eizasai@users.noreply.github.com>
2021-12-09 15:42:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,56 +13,94 @@ schedule.every().day.at("21:59").do(graphing.plot_today)
 schedule.every().day.at("22:00").do(emailsender.emaildaily)
 schedule.every().monday.do(graphing.plot_week)
 schedule.every().monday.do(emailsender.emailweek)
-
 def check():  
     Cane_fall=False
-    sc=0
-    oc=0
-    fg=0
-    hr=0
+    Cane_fall2=False
+    stillcounter=0
+    orientationcounter=0
+    forcegauge=0
+    indexoffall=0
     while thread1.is_alive():
         compressed=pd.read_csv("data.csv",chunksize=100000,parse_dates = True)
         csv=pd.concat(compressed)
         length=len(csv.index)
         l=length-1
-        if Cane_fall:
-            while l>length-600 and l>3:
-                b2=csv.iloc[l][" accelerometer1x"]
-                b1=csv.iloc[l-1]["accelerometer1x"]
-                c2=csv.iloc[l][" accelerometer1y"]
-                c1=csv.iloc[l-1][" accelerometer1y"]
-                d2=csv.iloc[l][" accelerometer1z"]
-                d1=csv.iloc[l-1][" accelerometer1z"]
-                f=csv.iloc[l][" strain guage"]
-                h=csv.iloc[l][" HR"]
-                bm=b1-b2
-                cm=c1-c2
-                dm=d1-d2
-                if bm<0.1 and cm<0.1 and dm<0.1:
-                    sc+=1
-                if b2<3:
-                    oc+=1     
-                if f>0.4:
-                    fg+=1
-                if h>40:
-                    hr+=1
-            if fg>100 or hr>100:
+        if Cane_fall and not Cane_fall2:
+            l3=l
+            l2=index-100
+            if l2<2:
+                l2=2
+            forcecounter=0
+            while l2<indexoffall:
+                f=float(csv.iloc[l2][" strain guage"])
+                if abs(f)>3:
+                    forcecounter+=1
+                # if forcecounter>19:
+                #     l2+=300
+                #     Cane_fall2=True  
+                #     print("fall stage 1 cleared")
+                l2+=1
+            if forcecounter>19:
+                l2+=300
+                print("fc:"+str(forcecounter))
+                Cane_fall2=True  
+                print("fall stage 1 cleared")
+            if not Cane_fall2:
+                print("fc:"+str(forcecounter))
                 Cane_fall=False
-            elif (sc>499 and oc>499):
+                print("fall cancelled")
+            else:
+                print("waiting 15 seconds")
+                time.sleep(15)
+        if Cane_fall2 and Cane_fall and l>l3:                
+            while l>l3 and l>3:
+                b2=float(csv.iloc[l][" accelerometer1x"])
+                b1=float(csv.iloc[l-1][" accelerometer1x"])
+                c2=float(csv.iloc[l][" accelerometer1y"])
+                c1=float(csv.iloc[l-1][" accelerometer1y"])
+                d2=float(csv.iloc[l][" accelerometer1z"])
+                d1=float(csv.iloc[l-1][" accelerometer1z"])
+                f=float(csv.iloc[l][" strain guage"])
+                bm=abs(b1-b2)
+                cm=abs(c1-c2)
+                dm=abs(d1-d2)
+                th=0.15
+                if bm<th and cm<th and dm<th:
+                    stillcounter+=1
+                if abs(float(b2))<5.2:
+                    orientationcounter+=1     
+                if abs(f)>3:
+                    forcegauge+=1
+                # if fg>50: #or hr>100:
+                #     Cane_fall=False
+                #     l-=600
+                #     print("fall detection cancelled2")
+                l-=1
+            if forcegauge>40: #or hr>100:
+                Cane_fall=False
+                print("fall detection cancelled2")    
+            print("fg:"+str(fg))
+            print("oc:"+str(oc))
+            print("sc:"+str(sc))
+            if (stillcounter>99 or orientationcounter>99) and Cane_fall:
                 graphing
+                print("fall confirmed")
                 sms("Fall detected")
                 emailsender.emailtool("Fall data")
-            sc=0
-            oc=0
-            fg=0
+            elif not forcegauge>50:
+                print("fall detection cancelled3")
+            stillcounter=0
+            orientationcounter=0
+            forcegauge=0
             Cane_fall=False
-        while l>3 and l>length-40:
-            b2=csv.iloc[l][" accelerometer1x"]
-            b1=csv.iloc[l-1][" accelerometer1x"]
-            c2=csv.iloc[l][" accelerometer1y"]
-            c1=csv.iloc[l-1][" accelerometer1y"]
-            d2=csv.iloc[l][" accelerometer1z"]
-            d1=csv.iloc[l-1][" accelerometer1z"]
+            Cane_fall2=False
+        while l>3 and l>length-150 and not Cane_fall:
+            b2=float(csv.iloc[l][" accelerometer1x"])
+            b1=float(csv.iloc[l-1][" accelerometer1x"])
+            c2=float(csv.iloc[l][" accelerometer1y"])
+            c1=float(csv.iloc[l-1][" accelerometer1y"])
+            d2=float(csv.iloc[l][" accelerometer1z"])
+            d1=float(csv.iloc[l-1][" accelerometer1z"])
             bm=b1-b2
             cm=c1-c2
             dm=d1-d2
@@ -74,18 +112,20 @@ def check():
                 r2=(c1-d1)/(dm-cm)
             else:
                 r2=0
+            r1=abs(r1)
+            r2=abs(r2)
             if r1<=1 and r2<=1:
                 if (r1>0 and r2>0) or (b1==c1 and b1==d1):
                     print(l)
                     print("fall detected")
-                    time.sleep(30)
+                    index=l
                     Cane_fall=True
                     l-=40
             l-=1
         time.sleep(0.5)
         
 def cane():
-    arduino=serial.Serial('COM6',115200) #'COM6' will vary so check your bluetooth settings. Also Macbook ports are notated differently than windows
+    arduino=serial.Serial('COM6',115200)
     filename="data.csv"
     if not exists(filename):
         open(filename,"w").write("time, HR, accelerometer1x, accelerometer1y, accelerometer1z, strain guage, accelerometer2x, accelerometer2y, accelerometer2z\n")
@@ -96,13 +136,18 @@ def cane():
         getData=str(arduino.readline())
         now=str(datetime.now())
         data=str(getData)[2:len(getData)-5]
-        #print(data)
+        # print(data)
         file=open(filename, "a")
         file.write(now+","+data+"\n")
         file.close()
         line+=1
         schedule.run_pending()    
     arduino.close()
+
+thread1=threading.Thread(target=cane)
+thread2=threading.Thread(target=check)
+thread1.start()
+thread2.start()
 
 thread1=threading.Thread(target=cane)
 thread2=threading.Thread(target=check)
